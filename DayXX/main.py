c3a14e4ff5b51912bf7ef8af6f434098c6b3d9fc
DECUP Louis <louis.decup@orange.fr>
2025-12-01 15:42:26 +0100

    Updated DayXX template

---
+++ DayXX/main.py
@@ -0,0 +1,21 @@
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
+logger = logging.getLogger(__name__)
+
+mainInput = "input.txt"
+testInput = "testinput.txt"
+txtInput = testInput
+
+logger.info(f"Running on {txtInput}")
+inPath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                      txtInput)
+inFile = open(inPath, 'r')
+inData = inFile.readlines()
+inFile.close()
+
+data = []
+for line in inData:
+    data.append(line.rstrip('\n'))
+logger.info(f"Successfully imported {len(data)} lines of data")
