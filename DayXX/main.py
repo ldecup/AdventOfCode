ce504b9ad1d1d8b33fbe8dc0ab9cd76ae6373e45
DECUP Louis <louis.decup@orange.fr>
2025-12-02 14:18:52 +0100

    Updated template with testing

--- DayXX/main.py
+++ DayXX/main.py
@@ -1,21 +1,25 @@
 import os
 import logging
 
-logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
-logger = logging.getLogger(__name__)
+def main():
+    logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
+    logger = logging.getLogger(__name__)
 
-mainInput = "input.txt"
-testInput = "testinput.txt"
-txtInput = testInput
+    mainInput = "input.txt"
+    testInput = "testinput.txt"
+    txtInput = testInput
 
-logger.info(f"Running on {txtInput}")
-inPath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                      txtInput)
-inFile = open(inPath, 'r')
-inData = inFile.readlines()
-inFile.close()
+    logger.info(f"Running on {txtInput}")
+    inPath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        txtInput)
+    inFile = open(inPath, 'r')
+    inData = inFile.readlines()
+    inFile.close()
 
-data = []
-for line in inData:
-    data.append(line.rstrip('\n'))
-logger.info(f"Successfully imported {len(data)} lines of data")
+    data = []
+    for line in inData:
+        data.append(line.rstrip('\n'))
+    logger.info(f"Successfully imported {len(data)} lines of data")
+
+if __name__ == "__main__":
+    main()
---
+++ DayXX/main_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from main import functionName
+
+class test_functionName(unittest.TestCase):
+    def testA(self):
+        self.assertEqual(functionName(a, b), result)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
