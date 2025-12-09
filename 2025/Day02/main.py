7dde740b624cccc1f0aae29a65303595fd86cef7
DECUP Louis <louis.decup@orange.fr>
2025-12-09 15:44:31 +0100

    Day 2 part 2

--- 2025/Day02/main.py
+++ 2025/Day02/main.py
@@ -1,10 +1,19 @@
 import os
 import logging
+import re
 
-def hasRangeInvalidIDs(testedRange):
+def hasRangeInvalidIDsP1(testedRange):
     invalidIDSum = 0
     for i in range(int(testedRange[0]), int(testedRange[1]) + 1):
         if str(i)[:int(len(str(i))/2)] == str(i)[int(len(str(i))/2):]:
+            invalidIDSum += i
+    return invalidIDSum
+
+def hasRangeInvalidIDsP2(testedRange):
+    regexp2 = r"^(\d+)\1+$" #Look for a pattern of 1 or more digits, that repeat, at least once, spanning between the start and the end of the string
+    invalidIDSum = 0
+    for i in range(int(testedRange[0]), int(testedRange[1]) + 1):
+        if len(re.findall(regexp2,str(i))) != 0:
             invalidIDSum += i
     return invalidIDSum
 
@@ -23,14 +32,17 @@ def main():
     inData = inFile.readlines()
     inFile.close()
 
-    totalInvalidIDs = 0
+    totalInvalidIDsP1 = 0
+    totalInvalidIDsP2 = 0
 
     data = inData[0].rstrip('\n').split(",")
     for index, datum in enumerate(data):
         data[index] = datum.split("-")
-        totalInvalidIDs += hasRangeInvalidIDs(data[index])
+        totalInvalidIDsP1 += hasRangeInvalidIDsP1(data[index])
+        totalInvalidIDsP2 += hasRangeInvalidIDsP2(data[index])
 
-    logger.info(f"Total sum of invalid IDs: {totalInvalidIDs}")
+    logger.info(f"Total sum of invalid IDs for part 1: {totalInvalidIDsP1}")
+    logger.info(f"Total sum of invalid IDs for part 2: {totalInvalidIDsP2}")
 
 if __name__ == "__main__":
     main()
--- 2025/Day02/main_test.py
+++ 2025/Day02/main_test.py
@@ -1,18 +1,33 @@
 import unittest
 
-from main import hasRangeInvalidIDs
+from main import hasRangeInvalidIDsP1
+from main import hasRangeInvalidIDsP2
 
-class test_hasRangeInvalidIDs(unittest.TestCase):
+class test_hasRangeInvalidIDsP1(unittest.TestCase):
     def testA(self):
-        self.assertEqual(hasRangeInvalidIDs(("11","22")), 33)
+        self.assertEqual(hasRangeInvalidIDsP1(("11","22")), 33)
     def testB(self):
-        self.assertEqual(hasRangeInvalidIDs(("95","115")), 99)
+        self.assertEqual(hasRangeInvalidIDsP1(("95","115")), 99)
     def testC(self):
-        self.assertEqual(hasRangeInvalidIDs(("998","1012")), 1010)
+        self.assertEqual(hasRangeInvalidIDsP1(("998","1012")), 1010)
     def testD(self):
-        self.assertEqual(hasRangeInvalidIDs(("1188511880","1188511890")), 1188511885)
+        self.assertEqual(hasRangeInvalidIDsP1(("1188511880","1188511890")), 1188511885)
     def testE(self):
-        self.assertEqual(hasRangeInvalidIDs(("2121212118","2121212124")), 0)
+        self.assertEqual(hasRangeInvalidIDsP1(("2121212118","2121212124")), 0)
+
+class test_hasRangeInvalidIDsP2(unittest.TestCase):
+    def testA(self):
+        self.assertEqual(hasRangeInvalidIDsP2(("11","22")), 33)
+    def testB(self):
+        self.assertEqual(hasRangeInvalidIDsP2(("95","115")), 210)
+    def testC(self):
+        self.assertEqual(hasRangeInvalidIDsP2(("998","1012")), 2009)
+    def testD(self):
+        self.assertEqual(hasRangeInvalidIDsP2(("1188511880","1188511890")), 1188511885)
+    def testE(self):
+        self.assertEqual(hasRangeInvalidIDsP2(("2121212118","2121212124")), 2121212121)
+    def testF(self):
+        self.assertEqual(hasRangeInvalidIDsP2(("565653","565659")), 565656)
 
 
 
